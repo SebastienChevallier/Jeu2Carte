f08e5de56ddc99a9efa05dd1269c860164606651
Seb <S.chevallier.pro@gmail.com>
2022-02-17 19:27:14 +0100

    BaseStructure

---
+++ Assets/Scripts/EffetsEntity.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EffetsEntity
+{
+	public string name;
+	public int price;
+	public string type;
+	public EnumEffets.enumEffets effets;
+	public int metric1;
+	public int metric2;
+	public int duree;
+	public int rarete;
+}
