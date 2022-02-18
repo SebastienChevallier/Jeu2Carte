eebc4d96cc80a72d1ffb469aeaad2ebbbdb999ed
Drawnight01 <71324732+Drawnight01@users.noreply.github.com>
2022-02-18 17:01:36 +0100

    So_Personage V2

---
+++ Assets/Scripts/SO_Personnages.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Personnage", menuName = "ScriptableObjects/Personnage", order = 1)]
+public class SO_Personnages : ScriptableObject
+{
+    public string name;
+
+    public string classe;
+
+    public int PV;
+    public int attPhys;
+    public int attMag;
+    public int defPhys;
+    public int defMag;
+
+}
