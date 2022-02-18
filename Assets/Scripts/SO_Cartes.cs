41f73e5a6b08870bf94100ba82ffeb308679d3a2
Seb <S.chevallier.pro@gmail.com>
2022-02-18 19:02:31 +0100

    Prefab cartes

--- Assets/Scripts/SO_Cartes.cs
+++ Assets/Scripts/SO_Cartes.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Cartes", menuName = "ScriptableObjects/Cartes", order = 1)]
 public class SO_Cartes : ScriptableObject
 {
+	[Header("Proprieté de la carte : ")]
 	public string _name;
 	public int price;
 	public string type;
-	public EnumEffets.enumEffets effet1;
-	public EnumEffets.enumEffets effet2;
-	
-	public int valeur1;
-	public int valeur2;
-	public int duree1;
-	public int duree2;
 	public int rarete;
 	public string classe;
+	public Sprite image;
+	public string description;
+
+	[Header("Valeur Effet 1")]
+	public EnumEffets.enumEffets effet1;
+	public int valeur1;
+	public int duree1;
+
+	[Header("Valeur Effet 2")]
+	public EnumEffets.enumEffets effet2;	
+	public int valeur2;	
+	public int duree2;
+	
+
+
 }
--- Assets/Scripts/_CartesManager.cs
+++ Assets/Scripts/_CartesManager.cs
@@ -1,36 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class _CartesManager : MonoBehaviour
 {
-	[Header("Proprieté de la carte : ")]
+	[HideInInspector]
 	public string _name;
+	[HideInInspector]
 	public int price;
+	[HideInInspector]
 	public string type;
+	[HideInInspector]
 	public int rarete;
+	[HideInInspector]
+	public string classe;
+
+	[Header("Textes Infos cartes : ")]
+	public TextMeshPro t_Nom;
+	public TextMeshPro t_Type;
+	public TextMeshPro t_Rarete;
+	public TextMeshPro t_Effet1;
+	public TextMeshPro t_Effet2;
+	public TextMeshPro t_Classe;
 
 	[Header("Scripts Refs")]
 	public SO_Cartes carteRef;
 	public EnumEffets scriptEffet;
 	public GameObject cible;
 
-	[Header("Value Effet 1 : ")]
+	[HideInInspector]
 	public EnumEffets.enumEffets effet1;
+	[HideInInspector]
 	public int valeur1;
+	[HideInInspector]
 	public int duree1;
 
-	[Header("Value Effet 2 : ")]
-	public EnumEffets.enumEffets effet2;	
-	public int valeur2;	
-	public int duree2;	
 
-	
+	[HideInInspector]
+	public EnumEffets.enumEffets effet2;
+	[HideInInspector]
+	public int valeur2;
+	[HideInInspector]
+	public int duree2;
+	[HideInInspector]
+
+
 
 	// Start is called before the first frame update
 	void Start()
     {
-		scriptEffet = GameObject.Find("GAMEMANAGER").GetComponent<EnumEffets>();
+		scriptEffet = GameObject.Find("EffetsManager").GetComponent<EnumEffets>();
 		CreateCard(carteRef);
 
 		scriptEffet.DoEffect(effet1, valeur1, duree1, rarete, cible);
@@ -44,12 +64,20 @@ public class _CartesManager : MonoBehaviour
 		type = carte.type;
 		effet1 = carte.effet1;
 		effet2= carte.effet2;
+		classe = carte.classe;
 		
 		valeur1 = carte.valeur1;
 		valeur2 = carte.valeur2;
 		duree1 = carte.duree1;
 		duree2 = carte.duree2;
 		rarete = carte.rarete;
+
+		t_Nom.text = _name;
+		t_Type.text = type;
+		t_Rarete.text = rarete.ToString();
+		t_Effet1.text = effet1.ToString() + " : inflige " + valeur1 + " pts de degats sur " + duree1 + " tours.";
+		t_Effet2.text = effet2.ToString() + " : inflige " + valeur2 + " pts de degats sur " + duree2 + " tours.";
+		t_Classe.text = classe;
     }
 
 
