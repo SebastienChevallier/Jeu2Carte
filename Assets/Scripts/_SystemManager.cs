7dad42f7a3c10cbc66d173f5b453fcf92efc3cc6
Drawnight01 <71324732+Drawnight01@users.noreply.github.com>
2022-02-23 02:42:35 +0100

    Mise en place validation des dégats

--- Assets/Scripts/_SystemManager.cs
+++ Assets/Scripts/_SystemManager.cs
@@ -14,6 +14,8 @@ public class _SystemManager : MonoBehaviour
     public float maxRand = 1.1f;
     private float previsuMaxPV, previsuMinPV;
     private float previsuMana;
+    private float actualDegats;
+    private float cost;
 
     //Stats Characters
     public _PersonnagesManager scriptPersoAttacker;
@@ -23,10 +25,9 @@ public class _SystemManager : MonoBehaviour
     //UI var
     public Image pvBar, pvBarMin, pvBarMax, manaBar, manaBarPrevisu;
     private TextMeshProUGUI tmp_name, tmp_pv, tmp_mana, tmp_attphys, tmp_attmag, tmp_defphys, tmp_defmag, tmp_vitesse, tmp_tauxcc;
+    private TextMeshProUGUI tmp_nameEnemy, tmp_pvEnemy, tmp_manaEnemy, tmp_attphysEnemy, tmp_attmagEnemy, tmp_defphysEnemy, tmp_defmagEnemy, tmp_vitesseEnemy, tmp_tauxccEnemy;
     void Start() 
     {
-        PhysicalAtt(50, 10, scriptPersoAttacker.attMag, scriptPersoTarget.defMag, scriptPersoAttacker.tauxCC);
-
         //UI
         tmp_name = GameObject.Find("PrevisuPlayer").transform.GetChild(0).GetComponent<TextMeshProUGUI>();
         tmp_pv = GameObject.Find("PrevisuPlayer").transform.GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>();
@@ -37,51 +38,97 @@ public class _SystemManager : MonoBehaviour
         tmp_defmag = GameObject.Find("PrevisuPlayer").transform.GetChild(6).GetChild(0).GetComponent<TextMeshProUGUI>();
         tmp_vitesse = GameObject.Find("PrevisuPlayer").transform.GetChild(7).GetChild(0).GetComponent<TextMeshProUGUI>();
         tmp_tauxcc = GameObject.Find("PrevisuPlayer").transform.GetChild(8).GetChild(0).GetComponent<TextMeshProUGUI>();
+
+        tmp_nameEnemy = GameObject.Find("PrevisuEnemy").transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        tmp_pvEnemy = GameObject.Find("PrevisuEnemy").transform.GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>();
+        tmp_manaEnemy = GameObject.Find("PrevisuEnemy").transform.GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+        tmp_attphysEnemy = GameObject.Find("PrevisuEnemy").transform.GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>();
+        tmp_defphysEnemy = GameObject.Find("PrevisuEnemy").transform.GetChild(4).GetChild(0).GetComponent<TextMeshProUGUI>();
+        tmp_attmagEnemy = GameObject.Find("PrevisuEnemy").transform.GetChild(5).GetChild(0).GetComponent<TextMeshProUGUI>();
+        tmp_defmagEnemy = GameObject.Find("PrevisuEnemy").transform.GetChild(6).GetChild(0).GetComponent<TextMeshProUGUI>();
+        tmp_vitesseEnemy = GameObject.Find("PrevisuEnemy").transform.GetChild(7).GetChild(0).GetComponent<TextMeshProUGUI>();
+        tmp_tauxccEnemy = GameObject.Find("PrevisuEnemy").transform.GetChild(8).GetChild(0).GetComponent<TextMeshProUGUI>();
     }
 
     void FixedUpdate() 
     {  
-
         //UI
-        pvBarMax.fillAmount = 1 - (previsuMaxPV / scriptPersoTarget.basePV);
-        pvBarMin.fillAmount = 1 - (previsuMinPV / scriptPersoTarget.basePV);
-        manaBarPrevisu.fillAmount = 1 - (previsuMana / scriptPersoAttacker.baseMana);
-
         tmp_name.text = scriptPersoAttacker._name;
         tmp_pv.text = scriptPersoAttacker.actualPV.ToString();
-        tmp_mana.text = previsuMana.ToString();
+        tmp_mana.text = scriptPersoAttacker.actualMana.ToString();
         tmp_attphys.text = scriptPersoAttacker.attPhys.ToString();
         tmp_defphys.text = scriptPersoAttacker.defPhys.ToString();
         tmp_attmag.text = scriptPersoAttacker.attMag.ToString();
         tmp_defmag.text = scriptPersoAttacker.defMag.ToString();
         tmp_vitesse.text = scriptPersoAttacker.vitesse.ToString();
         tmp_tauxcc.text = (scriptPersoAttacker.tauxCC * 100).ToString() + " %";
+
+        tmp_nameEnemy.text = scriptPersoTarget._name;
+        tmp_pvEnemy.text = scriptPersoTarget.actualPV.ToString();
+        tmp_manaEnemy.text = scriptPersoTarget.actualMana.ToString();
+        tmp_attphysEnemy.text = scriptPersoTarget.attPhys.ToString();
+        tmp_defphysEnemy.text = scriptPersoTarget.defPhys.ToString();
+        tmp_attmagEnemy.text = scriptPersoTarget.attMag.ToString();
+        tmp_defmagEnemy.text = scriptPersoTarget.defMag.ToString();
+        tmp_vitesseEnemy.text = scriptPersoTarget.vitesse.ToString();
+        tmp_tauxccEnemy.text = (scriptPersoTarget.tauxCC * 100).ToString() + " %";
     }
-    public void PhysicalAtt (int power, int cost, int Att, int Def, float tauxCC) 
+
+    public void Attack (int power, int cost, int Att, int Def, float tauxCC) 
     {
         rand = Random.Range(minRand, maxRand);
         float minDegats = (power * Att * modifier * minRand) / Def;
         float maxDegats = (power * Att * modifier * maxRand) / Def;
-        float actualDegats;
 
         float checkCC = Random.Range(0.0f, 1.0f);
         if (checkCC <= tauxCC)
         {
             actualDegats = (power * Att * modifier * rand * DCC) / Def; 
-            Debug.Log("Crit !!!");
+            Debug.Log("CRIT !!!");
         }
         else actualDegats = (power * Att * modifier * rand) / Def;
 
-        previsuMinPV = (scriptPersoTarget.actualPV - minDegats);
-        previsuMaxPV = (scriptPersoTarget.actualPV - maxDegats);
+        previsuMinPV = scriptPersoTarget.actualPV - minDegats;
+        previsuMaxPV = scriptPersoTarget.actualPV - maxDegats;
 
-        previsuMana = (scriptPersoAttacker.actualMana - cost);
-
-        //if validation
-        scriptPersoTarget.actualPV -= Mathf.RoundToInt(actualDegats);
-        if (scriptPersoTarget.actualPV <= 0) Death(target); 
+        previsuMana = scriptPersoAttacker.actualMana - cost;
     }
-    //MagicalAtt!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+    public void OnClickValidate ()
+    {
+        if (scriptPersoAttacker.actualMana >= cost)
+        {
+            scriptPersoAttacker.actualMana -= Mathf.RoundToInt(cost);
+            scriptPersoTarget.actualPV -= Mathf.RoundToInt(actualDegats);
+            if (scriptPersoTarget.actualPV <= 0) Death(target);
+
+            //UI
+            pvBar.fillAmount -= actualDegats / scriptPersoTarget.basePV;
+            manaBar.fillAmount -= cost / scriptPersoAttacker.baseMana;
+            pvBarMax.fillAmount = 0;
+            pvBarMin.fillAmount = 0;
+            manaBarPrevisu.fillAmount = 0;
+        }
+    }
+
+    public void OnClickCancel () 
+    {
+        pvBarMax.fillAmount = 0;
+        pvBarMin.fillAmount = 0;
+        manaBarPrevisu.fillAmount = 0;
+    }
+
+    public void OnClickTest ()
+    {
+        cost = 6;
+
+        Attack(10, Mathf.RoundToInt(cost), scriptPersoAttacker.attMag, scriptPersoTarget.defMag, scriptPersoAttacker.tauxCC);
+
+        pvBarMax.fillAmount = 1 - (previsuMaxPV / scriptPersoTarget.basePV);
+        pvBarMin.fillAmount = 1 - (previsuMinPV / scriptPersoTarget.basePV);
+        manaBarPrevisu.fillAmount = 1 - (previsuMana / scriptPersoAttacker.baseMana);
+    }
+
     private void Death (GameObject target) 
     {
         GameObject.Destroy(target);
